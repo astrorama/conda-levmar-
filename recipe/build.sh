287b02bfb7fb9d8d3ec27fa91213922acbb703cc
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2019-09-27 12:11:46 +0200

    Revert "Fix installation"
    This reverts commit b479b31a23f956241bc99b82f4d5ddf382e925e7.
    Use mkdir instead (-D -t are not portable)

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,8 +7,9 @@ else
 fi
 
 make -f Makefile.so CC=${CC}
-install -m 644 -t "${PREFIX}/include" -D "levmar.h"
-install -m 644 -t "${PREFIX}/lib" -D "liblevmar.${DYN}.2.2"
+mkdir -p "${PREFIX}/include/" "${PREFIX}/lib/"
+install -m 644 "levmar.h" "${PREFIX}/include/"
+install -m 644 "liblevmar.${DYN}.2.2" "${PREFIX}/lib/"
 ln -s "liblevmar.${DYN}.2.2" "${PREFIX}/lib/liblevmar.${DYN}.2"
 ln -s "liblevmar.${DYN}.2" "${PREFIX}/lib/liblevmar.${DYN}"
 
