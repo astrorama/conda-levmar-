b479b31a23f956241bc99b82f4d5ddf382e925e7
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2019-09-27 11:56:26 +0200

    Fix installation

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,8 +7,8 @@ else
 fi
 
 make -f Makefile.so CC=${CC}
-install -m 644 "levmar.h" "${PREFIX}/include"
-install -m 644 "liblevmar.${DYN}.2.2" "${PREFIX}/lib"
+install -m 644 -t "${PREFIX}/include" -D "levmar.h"
+install -m 644 -t "${PREFIX}/lib" -D "liblevmar.${DYN}.2.2"
 ln -s "liblevmar.${DYN}.2.2" "${PREFIX}/lib/liblevmar.${DYN}.2"
 ln -s "liblevmar.${DYN}.2" "${PREFIX}/lib/liblevmar.${DYN}"
 
