27ac6cba72a37ed87c0c8fecbfd0e15544eaf254
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2020-02-06 14:18:13 +0100

    Fix build on MacOSX

--- recipe/build.sh
+++ recipe/build.sh
@@ -28,12 +28,13 @@ mkdir -p "${PREFIX}/include/" "${PREFIX}/lib/" "${PREFIX}/bin/"
 install -m 644 "${SRC_DIR}/levmar.h" "${PREFIX}/include/"
 
 if [[ -n "${OSX_ARCH}" ]]; then
-    install -m 644 "liblevmar.2.6.dylib" "${PREFIX}/lib"
-    install -m 644 "liblevmar.dylib" "${PREFIX}/lib"
+    install -m 755 "liblevmar.2.6.dylib" "${PREFIX}/lib/"
+    ln -s "liblevmar.2.6.dylib" "${PREFIX}/lib/liblevmar.dylib"
+    install_name_tool -change "$(pwd)/liblevmar.2.6.dylib" "${PREFIX}/lib/liblevmar.2.6.dylib" lmdemo
 else
-    install -m 644 "liblevmar.so.2.6" "${PREFIX}/lib/"
+    install -m 755 "liblevmar.so.2.6" "${PREFIX}/lib/"
     ln -s "liblevmar.so.2.6" "${PREFIX}/lib/liblevmar.so.2"
     ln -s "liblevmar.so.2" "${PREFIX}/lib/liblevmar.so"
 fi
 
-install -m 755 "lmdemo" "${PREFIX}/bin"
+install -m 755 "lmdemo" "${PREFIX}/bin/"
